80ec942e958a1721a5faa5d8c6ad9cbdf0958083
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2023-11-22 13:01:40 +0100

    initial commit

---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,4 @@
+insert into student(id,firstname,lastname,email_address) values(11, 'Student', 'One', 'student@gmail.com')
+insert into student(id,firstname,lastname,email_address) values(12, 'Student', 'Two', 'student@gmail.com')
+insert into student(id,firstname,lastname,email_address) values(13, 'Student', 'Three', 'student@gmail.com')
+insert into student(id,firstname,lastname,email_address) values(14, 'Student', 'Four', 'student@gmail.com')
